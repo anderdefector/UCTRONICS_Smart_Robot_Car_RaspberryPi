8fc46cd20c82e9eb6fee203c32af8fb98a8ce203
andres <anderdefector@gmail.com>
2021-11-30 22:39:16 -0600

    Actualizacion

--- C_Mod/src/remoteserver.c
+++ C_Mod/src/remoteserver.c
@@ -227,7 +227,7 @@ void *fun1(void *arg) {
 
 void *fun2(void *arg) {
   //int IRVal = 0;
-  while (carstate.autoAvoid) {
+  while (carstate.autoAvoid == 1) {
     printf("Estoy en fun2 \n"); 
     usleep(1);
     //if (carstate.autoAvoid) {
