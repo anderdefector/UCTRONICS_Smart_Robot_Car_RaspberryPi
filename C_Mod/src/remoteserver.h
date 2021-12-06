cacdd8104d5bdab8c10424120a436fd596c4d1cf
andres <anderdefector@gmail.com>
2021-12-06 14:57:37 -0600

    Actualizacion

--- C_Mod/src/remoteserver.h
+++ C_Mod/src/remoteserver.h
@@ -83,9 +83,9 @@
 #define BUFFER_SIZE 10
 
 #define halfPWMPeriod   5000  //5ms
-#define forwardTime		1000000
+#define forwardTime		870000
 //#define  turnTime       500000    //500ms
-#define  turnTime         870000    //900ms
+#define  turnTime         250000    //900ms
 
 struct motionstate {
         unsigned int left:1;
