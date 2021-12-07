2bca312446ef015eff241b0fc8bfe46c42179bdf
andres <anderdefector@gmail.com>
2021-12-06 21:37:13 -0600

    Actualizacion

--- C_Mod/src/remoteserver.h
+++ C_Mod/src/remoteserver.h
@@ -85,7 +85,7 @@
 #define halfPWMPeriod   5000  //5ms
 #define forwardTime		870000
 //#define  turnTime       500000    //500ms
-#define  turnTime         100000    //900ms
+#define  turnTime         870000    //900ms
 
 struct motionstate {
         unsigned int left:1;
