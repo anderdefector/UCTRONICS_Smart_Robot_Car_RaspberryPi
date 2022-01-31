84bd62879d7c1115890ec3735322329969e21a14
andres <anderdefector@gmail.com>
2022-01-31 17:22:45 -0600

    Actualizacion

--- C/src/remoteserver.c
+++ C/src/remoteserver.c
@@ -307,10 +307,16 @@ int main(int argc, char *argv[])
 void *fun1(void *arg) {
   //unsigned char cnt = 0;
   //float dis = 0, temp_min = 0, temp_max = 0, temp_value = 0, temp = 0;
-  
+  int num1, num2, num3;
   while (1) {
     usleep(1);
     dis = disMeasure();
+    num1 = GET_GPIO(leftSensor);
+    num2 = GET_GPIO(middleSensor);
+    num3 = GET_GPIO(rightSensor);
+
+    printf("Izq: %d, Medio: %d, Der: %d \n ", num1, num2, num3);  
+
     if(carstate.distancia){
       buffer_enviar[0] = (int) dis;
       send(newsockfd , buffer_enviar , 1 , 0 );
