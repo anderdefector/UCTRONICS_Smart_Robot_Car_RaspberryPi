bbb58be6dbfaaff1adb371bc341c165cdfb89833
andres <anderdefector@gmail.com>
2022-01-31 17:32:57 -0600

    Actualizacion

--- C_Mod/src/remoteserver.c
+++ C_Mod/src/remoteserver.c
@@ -206,21 +206,17 @@ int main(int argc, char *argv[])
 void *fun1(void *arg) {
   unsigned char cnt = 0;
   float dis = 0, temp_min = 0, temp_max = 0, temp_value = 0, temp = 0;
+  int num1 = 0, num2 = 0, num3 = 0;
   while (1) {
     usleep(1);
     dis = disMeasure();
     printf("Distancia al objeto : %f \n", dis);
-    if (dis <  20.0) {
-      disWarning = 1;
-      printf("Hay objeto: \n");
-      //beepWarning();
-      if (carstate.forward) {
-        if (!(carstate.autoAvoid)) {
-          stop();
-        }
+    
+    num1 = GET_GPIO(leftSensor);
+    num2 = GET_GPIO(middleSensor);
+    num3 = GET_GPIO(rightSensor);
 
-      }
-    } else disWarning = 0;
+    printf("Izq: %d, Medio: %d, Der: %d \n ", num1, num2, num3);
 
   }
 }
