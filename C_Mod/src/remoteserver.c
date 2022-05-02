4cc0a396ccf4fc1cca594c849213d9a0b912826f
ander <anderdefector@gmail.com>
2022-05-02 10:31:04 -0500

    Actualizacion

--- C_Mod/src/remoteserver.c
+++ C_Mod/src/remoteserver.c
@@ -482,7 +482,7 @@ int updateCarMotion(void) {
   }else if(carstate.servoMovLat){
     carstate.servoMovLat = 0;
     strcpy(direction, "Moviendo cámara");
-    servoCtrl(servo_1,  430);
+    servoCtrl(servo_1,  420);
   }
 
   
